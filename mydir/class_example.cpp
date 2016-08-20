81366f7acd3ef899488dd16939caba91f745c364
Tigran <mtg388@mail.ru>
2016-08-20 22:32:19 +0400

    adding class example

---
+++ mydir/class_example.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+ 
+class DateClass 
+{
+    int m_month; 
+    int m_day; 
+    int m_year; 
+ 
+public:
+    void setDate(int month, int day, int year) 
+    {
+        m_month = month;
+        m_day = day;
+        m_year = year;
+    }
+ 
+    void print() 
+    {
+        std::cout << m_month << "/" << m_day << "/" << m_year << std::endl  ;
+    }
+};
+ 
+int main()
+{
+    DateClass date;
+    date.setDate(20, 8, 2016); 
+    date.print(); 
+ 
+    return 0;
+}
